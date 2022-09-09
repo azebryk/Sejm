db8b43ec1563a05edd312801dbbe7a045c1cbbb5
agata <agatazebryk7@gmail.com>
2022-09-09 21:17:00 +0200

    Adding file for streamlit visualisation.

---
+++ Main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import datetime
+# from st_aggrid import AgGrid
+# from st_aggrid.grid_options_builder import GridOptionsBuilder
+
+st.write("Hello!")
